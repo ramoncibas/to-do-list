06149487e3fc0dad8375b3d2eb5c2f11c4cd50cc
Ramon Cibas <ramon.cibas@hotmail.com>
2021-06-24 21:17:34 -0300

    task component

---
+++ src/components/Tasks/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Feather } from "@expo/vector-icons";
+import Swipeable from "react-native-gesture-handler/Swipeable";
+
+import {
+  ContainerTask,
+  TextTask,
+  ContainerButton,
+  ActionContainer
+} from "./styles";
+
+export default function Tasks({ data, selectedItem, deleteItem }) {
+  const RightActions = () => {
+    return (
+      <ActionContainer onPress={() => deleteItem(data.id)}>
+        <Feather name="trash" color="#fff" size={24}/>
+      </ActionContainer>
+    );
+  };
+
+  return (
+    <ContainerTask>
+      <Swipeable renderRightActions={RightActions}>
+        <ContainerButton activeOpacity={0.8} onPress={() => selectedItem(data)}>
+          <TextTask>{data.titulo}</TextTask>
+        </ContainerButton>
+      </Swipeable>
+    </ContainerTask>
+  );
+}
---
+++ src/components/Tasks/styles.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const ContainerTask = styled.View``;
+
+export const ContainerButton = styled.TouchableOpacity`
+  flex-direction: row;
+  background-color: #fff;
+  
+  padding: 25px;
+  margin-bottom: 20px;
+
+  border-radius: 10px;
+  
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: row;
+`;
+
+export const TextTask = styled.Text`
+  max-width: 80%;
+`;
+
+export const ActionContainer = styled.TouchableOpacity`
+  width: 20%;  
+  height: 50px;
+  
+  margin: 7px 10px;
+
+  justify-content: center;
+  align-items: center;
+
+  background-color: #ff5555;
+  border-radius: 7px;
+`;
