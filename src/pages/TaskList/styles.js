d3db6a7340d90676e1344d45270a6f705677f69b
Ramon Cibas <ramon.cibas@hotmail.com>
2021-06-29 16:49:53 -0300

    updated style

--- src/pages/TaskList/styles.js
+++ src/pages/TaskList/styles.js
@@ -2,34 +2,48 @@ import styled from "styled-components";
 
 export const TaskListContainer = styled.View`
   flex: 1;
-  background-color: #EFEFEF;
-  padding: 80px 20px 0 20px;
+  background-color: #0d1117;
+  padding: 50px 20px 0 20px;
 `;
 
 export const Header = styled.View`  
   align-items: center;
   justify-content: space-between;
   flex-direction: row;
+  margin: 20px 0;
 `;
 
 export const Title = styled.Text`
-  font-size: 24px;
+  font-size: 26px;
   font-weight: bold;
+  color: #fff;
 `;
 
 export const ButtonTask = styled.TouchableOpacity`
+  position: absolute;
+  bottom: 20px;
+  right: 20px;
+
   width: 50px;
   height: 50px;
+
+  border: 1px solid #333;
   border-radius: 50px;
+
   background-color: #FFF;
   justify-content: center;
   align-items: center;
-  border-color: #C0C0C0;
-  border-width: 1px;
+  
 `;
 
-export const TitleTaskList = styled.Text`
+export const ContainerEmpty = styled.View`
+  margin-top: 15%;
+  align-items: center;
+`;
+
+export const TitleWarning = styled.Text`
   font-size: 18px;
+  color: #f1e05a;
 `;
 
 
