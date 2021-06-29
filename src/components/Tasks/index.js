a5a8b5ae0535afe43513ff3fba99df4dd76a7ebe
Ramon Cibas <ramon.cibas@hotmail.com>
2021-06-29 16:51:06 -0300

    index.js

--- src/components/Tasks/index.js
+++ src/components/Tasks/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Feather } from "@expo/vector-icons";
 import Swipeable from "react-native-gesture-handler/Swipeable";
-
+import { Text } from "react-native";
 import {
   ContainerTask,
   TextTask,
@@ -22,7 +22,7 @@ export default function Tasks({ data, selectedItem, deleteItem }) {
     <ContainerTask>
       <Swipeable renderRightActions={RightActions}>
         <ContainerButton activeOpacity={0.8} onPress={() => selectedItem(data)}>
-          <TextTask>{data.titulo}</TextTask>
+          <TextTask>{data.title}</TextTask>
         </ContainerButton>
       </Swipeable>
     </ContainerTask>
