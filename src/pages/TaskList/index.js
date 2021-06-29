ca32f9a3f0fc716e1bc8175528542c5e52d1e9f6
Ramon Cibas <ramon.cibas@hotmail.com>
2021-06-29 16:51:29 -0300

    activity indicator add

--- src/pages/TaskList/index.js
+++ src/pages/TaskList/index.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { Keyboard, KeyboardAvoidingView, Platform, TouchableOpacity, Modal} from "react-native";
+import { Modal, ActivityIndicator} from "react-native";
 import { Feather } from "@expo/vector-icons";
 
 import Tasks from "../../components/Tasks";
 import {
   TaskListContainer,
   Header,
-  Title,  
-  TitleTaskList,
+  Title,
+  ContainerEmpty,
+  TitleWarning,
   ButtonTask,
   ListItemTask,
 } from "./styles";
@@ -17,7 +18,8 @@ import ModalTask from "../../components/ModalTask";
 import { deleteTask, getTaskSave } from "../../utils/storeTask";
 
 export default function TaskList() {
-  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState({});
   const [modalVisible, setModalVisible] = useState(false);
   
   // Getting the tasks saved in local storage
@@ -25,6 +27,7 @@ export default function TaskList() {
     const getTask = async () => {
       const result = await getTaskSave("mytask");
       setData(result);
+      setLoading(false);
     }
 
     getTask();
@@ -34,34 +37,48 @@ export default function TaskList() {
   const handleItem = (item) => {
     setData(item);
     setModalVisible(true);
+    console.log(item);
   }
   
   // Delete Task from local storage
   const handleDelete = async (id) => {
     const result = await deleteTask(data, id);
-    setData(result);
+    setData(result);    
   }  
 
   return (
     <TaskListContainer>
       <Header>
-        <Title>Lista de Tarefas</Title>
-        <ButtonTask onPress={() => setModalVisible(true)}>
-          <Feather name="plus" color="#000" size={24}/>
-        </ButtonTask>
+        <Title>Lista de Tarefas</Title>        
       </Header>
       {
-        data == "" && <TitleTaskList>Você ainda não tem anda!</TitleTaskList>
+        loading && (
+          <ContainerEmpty>
+            <ActivityIndicator color="#fff" size={25}/>
+          </ContainerEmpty>
+        )
+      }
+
+      {
+        !loading && data == "" && (
+          <ContainerEmpty>
+            <TitleWarning>Você ainda não tem anda!</TitleWarning>
+          </ContainerEmpty>
+        )
       }
       <ListItemTask
-        data= { data }
-        keyExtractor={(item) => (item.id)}
-        renderItem={({ item }) => <Tasks data={item} selectedItem={ handleItem } deleteItem={ handleDelete }/>}
+        data={data}
+        keyExtractor={(item) => String(item.id)}
+        renderItem={({ item }) => <Tasks data={item} selectedItem={handleItem} deleteItem={handleDelete}/>}
         contentContainerStyle={{ paddingBottom: 20 }}
         showsVerticalScrollIndicator={false}
       />
 
-      <Modal visible={ modalVisible } transparent animationType="slide">
+      <ButtonTask onPress={() => setModalVisible(true)}>
+        <Feather name="plus" color="#000" size={24}/>
+      </ButtonTask>
+
+      <Modal visible={modalVisible} transparent animationType="slide">
         <ModalTask onClose={() => setModalVisible(false)} data={data} />
       </Modal>
     </TaskListContainer>
